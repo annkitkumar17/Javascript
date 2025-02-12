9c308caadfd3364325ac52cb7dfd6d5131428765
Ankit kumar <ankitcodes17@gmail.com>
2025-02-12 15:56:35 +0000

    Done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail ="ankit@gmail.com"
+var accountPassword= "54321"
+accountCity ="jaipur"
+// accountID="4" not allowed //
+
+
+accountEmail="shll@gmail.com"
+accountPassword="740754"
+accountCity="delhi"
+let accountState;
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
