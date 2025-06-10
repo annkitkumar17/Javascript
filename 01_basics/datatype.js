2491f2c7e70cb9b4f2f0ce33d04332c6f0f228ca
Ankit kumar <ankitcodes17@gmail.com>
2025-06-10 04:08:19 +0000

    container config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert( 3-3 ) //here using node js not the browser
+
+console.log(3+3); 
+
+console.log("Ankit")
+/* code redability should high */
+
+let name = "ankit"
+let age = "19"
+let men ="true"
+
+/* number = 2 to the power 53
+bigint
+stirng
+boolean
+null= standalone value
+undefined 
+symbol
+
+*/
+
+console.log(typeof undefined ) // output undefined
+console.log(typeof null  ) // object
